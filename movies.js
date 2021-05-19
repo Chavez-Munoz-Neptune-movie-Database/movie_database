109b1318a36eb36d67d796c7271efc0220090a42
Jose M Chavez <jose.m.chavez00@gmail.com>
2021-05-19 10:46:16 -0500

    Added RESTful API requests

--- movies.js
+++ movies.js
@@ -1,4 +1,20 @@
-// Adds movie to the database
+// Read movies in database
+fetch('https://beneficial-calm-rat.glitch.me/movies.', patchOptions).then(getMovies);
+
+let readOptions = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+$("#displayMovies").click(() => {
+    let inputVal = $("#id-to-display").val();
+    fetch(`https://beneficial-calm-rat.glitch.me/movies.${inputVal}`, readOptions)
+        .then(getMovies);
+});
+
+// Add movie to database
 $("#addmovie").click(() => {
     fetch('https://beneficial-calm-rat.glitch.me/movies.', postOptions)
         .then(getMovies);
@@ -15,5 +31,56 @@ $("#addmovie").click(() => {
                 }
             }
         })
-
 });
+
+// Delete Movie from database
+fetch('https://beneficial-calm-rat.glitch.me/movies.', patchOptions).then(getMovies);
+
+let deleteOptions = {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+$("#deleteMovie").click(() => {
+    let inputVal = $("#id-to-delete").val();
+    fetch(`https://beneficial-calm-rat.glitch.me/movies.${inputVal}`, deleteOptions)
+        .then(getMovies);
+});
+
+
+
+// Update Movie in database
+fetch('https://beneficial-calm-rat.glitch.me/movies.', patchOptions).then(getMovies);
+
+let updateOptions = {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+$("#updateMovies").click(() => {
+    let inputVal = $("#id-to-update").val();
+    fetch(`https://beneficial-calm-rat.glitch.me/movies.${inputVal}`, updateOptions)
+        .then(getMovies);
+});
+
+
+// View specific Movie
+fetch('https://beneficial-calm-rat.glitch.me/movies.', patchOptions).then(getMovies);
+
+let viewMovie = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+$("#searchMovie").click(() => {
+    let inputVal = $("#id-to-existing").val();
+    fetch(`https://beneficial-calm-rat.glitch.me/movies.${inputVal}`,viewMovie)
+        .then(getMovies);
+})
+
