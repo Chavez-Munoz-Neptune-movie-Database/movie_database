b7647538b46125a42e8bd85822a2c82e20ae0031
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-05-20 10:21:06 -0500

    continue part of movie project

--- movies.js
+++ movies.js
@@ -1,6 +1,6 @@
 // Jquery Page preloader
-$(window).ready(function(){
-    setTimeout(function() {
+$(window).ready(function () {
+    setTimeout(function () {
         $('.preloader').fadeOut();
     }, 2000);
 });
@@ -57,7 +57,6 @@ $("#deleteMovie").click(() => {
 });
 
 
-
 // Update Movie in database
 fetch('https://beneficial-calm-rat.glitch.me/movies.', patchOptions).then(getMovies);
 
@@ -87,8 +86,69 @@ let viewMovie = {
 
 $("#searchMovie").click(() => {
     let inputVal = $("#id-to-existing").val();
-    fetch(`https://beneficial-calm-rat.glitch.me/movies.${inputVal}`,viewMovie)
+    fetch(`https://beneficial-calm-rat.glitch.me/movies.${inputVal}`, viewMovie)
         .then(getMovies);
 })
 
+
+// ===HERE STAR THE PAGE CODE===//
+const line = document.querySelector('.container-carousel');
+const movies = document.querySelectorAll('.movies');
+
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
+
+
+// ? ----- ----- Event Listener para la left arrow. ----- -----
+rightArrow.addEventListener('click', () => {
+    line.scrollLeft += line.offsetWidth;
+
+    const activeIndicator = document.querySelector('.indicators .active');
+    if (activeIndicator.nextSibling) {
+        activeIndicator.nextSibling.classList.add('active');
+        activeIndicator.classList.remove('active');
+    }
+});
+
+// ? ----- ----- Event Listener para la right arrow. ----- -----
+leftArrow.addEventListener('click', () => {
+    line.scrollLeft -= line.offsetWidth;
+
+    const activeIndicator = document.querySelector('.indicators .active');
+    if (activeIndicator.previousSibling) {
+        activeIndicator.previousSibling.classList.add('active');
+        activeIndicator.classList.remove('active');
+    }
+});
+// ? ----- ----- icon button pages ----- -----
+const pagesNum = Math.ceil(movies.length / 5);
+for (let i = 0; i < pagesNum; i++) {
+    const indicator = document.createElement('button');
+
+    if (i === 0) {
+        indicator.classList.add('active');
+    }
+
+    document.querySelector('.indicators').appendChild(indicator);
+    indicator.addEventListener('click', (e) => {
+        line.scrollLeft = i * line.offsetWidth;
+
+        document.querySelector('.indicators .active').classList.remove('active');
+        e.target.classList.add('active');
+    });
+}
+// ? ----- ----- Hover ----- -----
+movies.forEach((movie) => {
+    movie.addEventListener('mouseenter', (e) => {
+        const element = e.currentTarget;
+        setTimeout(() => {
+            movies.forEach(movie => movie.classList.remove('hover'));
+            element.classList.add('hover');
+        }, 300);
+    });
+});
+
+line.addEventListener('mouseleave', () => {
+    movies.forEach(movie => movie.classList.remove('hover'));
+});
 
